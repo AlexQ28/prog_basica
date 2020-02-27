4128886caa4df52b392b8ae61b191747fe590e3b
AlexQ28 <aquezada010@gmail.com>
2020-02-26 18:47:50 -0600

    cambios 26Feb

---
+++ arreglo.js
@@ -0,0 +1,18 @@
+
+/*
+let flores = ['Rosa', 'Girasol', 'Lirio'];
+
+console.log(flores[2]);
+
+*/
+
+let personas = ['Fernado','Melissa', 'Maria'];
+let salarios = [1000, 1200, 3000]
+
+for ( let i = 0; i < personas.length; i++ ) {
+
+
+    console.log(personas[i] + ' tiene un salario de ' + salarios[i] );
+
+
+}
---
+++ function.js
@@ -0,0 +1,47 @@
+/*
+
+function sumar (a, b) {
+
+    return a + b;
+
+} 
+
+let total = sumar (10, 20);
+
+console.log('Total: ' + total);
+
+*/
+
+function imprimirTabla ( base, limite ) {
+    
+for (let i = 1; i <=limite; i++  ) {
+
+    let resultado = base * i
+    
+    console.log(base + ' x ' + i + ' = ' + resultado );
+
+}
+
+}
+
+imprimirTabla ( 10, 5 );
+imprimirTabla ( 3, 10 );
+
+/*
+FUNCIONES
+Una funcion es un conjunt de lineas de codigo que realizan 
+una tarea especifica y pueden retornar algo.
+
+Ejemplo: 
+function saludar () {
+   
+
+}
+
+LLamada a la funcion 
+saludar ();
+
+PARAMETRO Es algo que le podemos mandar a la funcion para que trabjae con el 
+
+
+*/
